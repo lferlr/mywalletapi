f29e0936e8ac032d3d701353892bfba94cc03ce1
Lucas Rodrigues <53006018+lferlr@users.noreply.github.com>
2023-06-24 00:56:50 -0300

    Adicionando campo no migrations

---
+++ MyWalletAPI.Infra/Migrations/20230621011512_AddInstallments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWalletAPI.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInstallments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Installments",
+                table: "Expense",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Installments",
+                table: "Expense");
+        }
+    }
+}
